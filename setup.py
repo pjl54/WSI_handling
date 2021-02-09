dfe4c30702cee448bd0f180d456cb1e66d66d978
pjl54 <40030915+pjl54@users.noreply.github.com>
2021-02-09 10:43:16 -0500

    update to 0.16

--- setup.py
+++ setup.py
@@ -8,15 +8,15 @@ with open(path.join(this_directory, 'README.md')) as f:
 setup(
   name = 'WSI_handling',         
   packages = ['WSI_handling'],   
-  version = '0.15',
+  version = '0.16',
   license='MIT',        
   description = 'Convienent handling of annotated whole slide images',
   long_description=long_description,
   long_description_content_type='text/markdown',
   author = 'Patrick Leo',                   
   author_email = 'pjl54@case.edu',      
-  url = 'https://github.com/pjl54/WSI_handling/tree/pip_ready',   
-  download_url = 'https://github.com/pjl54/WSI_handling/archive/v0.15.tar.gz',
+  url = 'https://github.com/pjl54/WSI_handling',   
+  download_url = 'https://github.com/pjl54/WSI_handling/archive/v0.16.tar.gz',
   keywords = ['whole slide image', 'digital pathology', 'annotations'],
   install_requires=[
           'numpy',
@@ -30,6 +30,6 @@ setup(
     'Development Status :: 3 - Alpha',
     'Intended Audience :: Developers',    
     'License :: OSI Approved :: MIT License',    
-    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.8',
   ],
 )
